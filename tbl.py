7ed4a0e74e82628a53462193b580fc0a885348dd
Super User <root@at.nt.org>
2026-05-18 12:42:34 +0100

    n_update

--- tbl.py
+++ tbl.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
 
+import pandas as pd
+import numpy as np
+import math
+
 #-----------------------------------------------
 #	learning github creating projects
 #-----------------------------------------------
+
 
 
 
