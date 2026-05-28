458a169f6d120d1091974206e7c6606e8958e1fd
Super User <root@at.nt.org>
2026-05-28 11:16:08 +0100

    schema update

--- tbl.py
+++ tbl.py
@@ -4,39 +4,29 @@
 #	python fedora linux
 #-----------------------------------------------
 
-#base api
+# base api
 
 #import fastapi
 #from fastapi import FastAPI as ft
-
 #app = ft()
 #@app.get('/')
 #def fnc():
 #	return{'ft':'apis'}
-
 
 # odfpy
 
 #import pandas as pd
 #import numpy as np
 #import math
-
-
 #df = pd.DataFrame(np.random.randn(24,42))
 #df.to_excel('tmp.ods')
-#rd = pd.read_excel('tmp.ods')
-#print(rd)
-
+#from ooodev.calc import Calc
 
 # html
 
-#df = pd.DataFrame(np.random.randn(42,24))
 #df.to_html('tmp.html')
-#rs = pd.read_html('tmp.html')
-#print(rs)
 
-
-# storages
+# dbs
 
 
 
